2e9b673333aa3cf97882d1b447179417d46e8a3a
Jiyun Jeong <wjdwldbsskfk@gmail.com>
2024-08-16 22:55:43 +0900

    서버 기본 설정

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
