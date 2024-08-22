56eca0efed9be61f35343c3e8c597d0a99c9b4fc
Jiyun Jeong <wjdwldbsskfk@gmail.com>
2024-08-22 14:38:40 +0900

    오류 핸들러 추가

--- routers/groupRouter.js
+++ routers/groupRouter.js
@@ -1,30 +1,54 @@
 import express from 'express';
 import * as groupController from '../controllers/groupController.js';
+import { Prisma } from '@prisma/client';
 
 const router = express.Router();
 
+function asyncHandler(handler) {
+    return async function (req, res) {
+      try {
+        await handler(req, res);
+      } catch (e) {
+        if (
+          e.name === 'StructError' ||
+          e instanceof Prisma.PrismaClientValidationError
+        ) {
+          res.status(400).send({ message: e.message });
+        } else if (
+          e instanceof Prisma.PrismaClientKnownRequestError &&
+          e.code === 'P2025'
+        ) {
+          res.sendStatus(404);
+        } else {
+          res.status(500).send({ message: e.message });
+        }
+      }
+    };
+  }
+  
+
 // 그룹 등록
-router.post("/", groupController.registerGroup);
+router.post("/", asyncHandler(groupController.registerGroup));
 
 // 그룹 목록 조회
-router.get("/", groupController.getGroups);
+router.get("/", asyncHandler(groupController.getGroups));
 
 // 그룹 수정
-router.put("/:groupId", groupController.updateGroup);
+router.put("/:groupId", asyncHandler(groupController.updateGroup));
 
 // 그룹 삭제
-router.delete("/:groupId", groupController.deleteGroup);
+router.delete("/:groupId", asyncHandler(groupController.deleteGroup));
 
 // 그룹 상세 정보 확인
-router.get("/:groupId", groupController.getGroupDetail);
+router.get("/:groupId", asyncHandler(groupController.getGroupDetail));
 
 // 그룹 조회 권한 확인
-router.post("/:groupId/verify-password", groupController.checkGroupAccess);
+router.post("/:groupId/verify-password", asyncHandler(groupController.checkGroupAccess));
 
 // 그룹 공감하기
-router.post("/:groupId/like", groupController.likeGroup);
+router.post("/:groupId/like", asyncHandler(groupController.likeGroup));
 
 // 그룹 공개 여부 확인
-router.get("/:groupId/is-public", groupController.checkGroupPublic);
+router.get("/:groupId/is-public", asyncHandler(groupController.checkGroupPublic));
 
 export default router;
