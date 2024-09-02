93c8dab0079cc6465d8a447c5a889ad100c810b4
Jiyun Jeong <wjdwldbsskfk@gmail.com>
2024-09-02 20:18:30 +0900

    답글 CRUD 생정

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ app.use(express.json());
 // 라우트 설정
 import groupRouter from './routers/groupRouter.js';
 import memoryRouter from './routers/memoryRouter.js';
+import commentRouter from './routers/commentRouter.js';
 
 
 app.use("/api/groups", groupRouter);
 app.use("/api/posts", memoryRouter);
+app.use("/api/comments", commentRouter);
 /////////////////////////////////////////
 
 app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
