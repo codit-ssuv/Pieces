f0160f677a266f96b7a8b93adc45a41b3046db21
Na young Heo <heonayoung14@gmail.com>
2024-09-03 17:23:43 +0900

    public 화면 구현

--- pieces/src/components/header.js
+++ pieces/src/components/header.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import '../styles/header.css';
 import logo from '../assets/logo.svg';
-import makeGroupIcon from '../assets/makeGroup.svg';
 import searchBar from '../assets/search.svg';
 
 const Header = () => {
     return (
         <header className="header">
-            <div className="logo">
-                <img src={logo} alt="조각집 로고" />
+            <div className="top-row">
+                <div className="logo">
+                    <img src={logo} alt="조각집 로고" />
+                </div>
+                <button className="cta-button">
+                    그룹 만들기 
+                </button>
             </div>
-            <div className="search-bar">
-                <img src={searchBar} alt="검색 아이콘" className="search-icon" />
-            
+            <div className="bottom-row">
+                <div className="search-bar">
+                    <img src={searchBar} alt="검색 아이콘" className="search-icon" />
+                </div>
+                <div className="dropdown">
+                    <select>
+                        <option value="option1">공감순</option>
+                        <option value="option2">옵션 2</option>
+                        <option value="option3">옵션 3</option>
+                    </select>
+                </div>
             </div>
-            <div className="filter">
-                <span>필터</span>
-                <select>
-                    <option value="option1">옵션 1</option>
-                    <option value="option2">옵션 2</option>
-                    <option value="option3">옵션 3</option>
-                </select>
-            </div>
-            <button className="cta-button">
-                <img src={makeGroupIcon} alt="그룹 만들기 아이콘" /> 
-            </button>
         </header>
     );
 };
--- pieces/src/index.js
+++ pieces/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Main from './pages/main'
-import "./styles/main.css"
+import Public from './pages/public';
+import "./styles/public.css";
+import Private from './pages/private';
+import "./styles/private.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
-        <Main />
+        <Public />
   </React.StrictMode>,
   document.getElementById('root')
 );
